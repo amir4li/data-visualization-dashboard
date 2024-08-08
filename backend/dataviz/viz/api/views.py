c6fb6a09988d0aa09d8a5b9c24156b63e2d4a1a5
Amir Ali <aamir4ly@gmail.com>
2024-08-08 22:37:16 +0530

    query filters added

--- backend/dataviz/viz/api/views.py
+++ backend/dataviz/viz/api/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from django_filters import rest_framework as filters
 from ..models import InsightData
 from ..serializers import InsightDataSerializer
-
 
 
 
@@ -16,8 +16,38 @@ from ..serializers import InsightDataSerializer
 def insight_list(request):
     if request.method == 'GET':
         insights = InsightData.objects.all()
+        
+        #filters
+        end_year = request.query_params.get('end_year')
+        topic = request.query_params.get('topic')
+        sector = request.query_params.get('sector')
+        region = request.query_params.get('region')
+        pestle = request.query_params.get('pestle')
+        source = request.query_params.get('source')
+        swot = request.query_params.get('swot')
+        country = request.query_params.get('country')
+        
+        if end_year:
+            insights = insights.filter(end_year=end_year)
+        if topic:
+            insights = insights.filter(topic__icontains=topic)
+        if sector:
+            insights = insights.filter(sector__icontains=sector)
+        if region:
+            insights = insights.filter(region__icontains=region)
+        if pestle:
+            insights = insights.filter(pestle__icontains=pestle)
+        if source:
+            insights = insights.filter(source__icontains=source)
+        if swot:
+            insights = insights.filter(swot__icontains=swot)
+        if country:
+            insights = insights.filter(country__icontains=country)
+
+        
         serializer = InsightDataSerializer(insights, many=True)
-        return Response(serializer.data)
+        data = { "length": len(serializer.data), "data": serializer.data}
+        return Response(data)
     
     elif request.method == 'POST':
         serializer = InsightDataSerializer(data=request.data)
